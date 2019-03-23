74d1c7d4bd2cd03c0e59ff39e6326c3fc8aa3c3e
KarenAmicone <karen_amicone@hotmail.com>
2019-03-22 20:31:26 -0600

    Puliendo el codigo

--- src/js/rutas.js
+++ src/js/rutas.js
@@ -2,9 +2,9 @@
         libreria.getID('vista').enrutar()
                 .ruta('/', './vistas/login.html', 'controlador', window.manejador.login)
                 //Este es el muro
-                .ruta('/muro', './vistas/muro.html', 'controlador', window.manejador.logOut)
+                .ruta('/muro', './vistas/muro.html', 'controlador', window.manejador.wall)
                 //Este es el perfil
-                .ruta('/perfil', './vistas/perfil.html', 'controlador',window.manejador.perfil)
+                .ruta('/perfil', './vistas/perfil.html', 'controlador',window.manejador.profile)
 
 
 })(window, document);
